73a37417a6bed322fb9c5026527d211a484b9d45
Eric Turcotte <when.the.music.play@gmail.com>
2014-08-21 22:16:29 -0400

    Increase rows returned to charts

--- application/classes/Model/Hour.php
+++ application/classes/Model/Hour.php
@@ -4,7 +4,7 @@ class Model_Hour {
 
     public function getTemperatureData() {
         // SELECT * FROM ( SELECT * FROM temperature ORDER BY id_temperature DESC LIMIT 1 ) sub ORDER BY id_temperature ASC
-        $query = DB::query(Database::SELECT, "SELECT datetime, room_temperature, tank_temperature FROM ( SELECT * FROM hour ORDER BY id_hour DESC LIMIT 20 ) sub ORDER BY id_hour ASC");
+        $query = DB::query(Database::SELECT, "SELECT datetime, room_temperature, tank_temperature FROM ( SELECT * FROM hour ORDER BY id_hour DESC LIMIT 40 ) sub ORDER BY id_hour ASC");
 
         //$query = DB::select('datetime', 'room_temperature', 'tank_temperature')->from('hour')->order_by('datetime', 'DESC')->limit(20)->offset(0);
         return $query->execute()->as_array();
--- application/classes/Model/QuarterHour.php
+++ application/classes/Model/QuarterHour.php
@@ -3,7 +3,7 @@
 class Model_QuarterHour {
 
     public function getSunlightData() {
-        $query = DB::query(Database::SELECT, "SELECT datetime, sunlight FROM ( SELECT * FROM quarter_hour ORDER BY id_quarter_hour DESC LIMIT 20 ) sub ORDER BY id_quarter_hour ASC");
+        $query = DB::query(Database::SELECT, "SELECT datetime, sunlight FROM ( SELECT * FROM quarter_hour ORDER BY id_quarter_hour DESC LIMIT 80 ) sub ORDER BY id_quarter_hour ASC");
 
         //$query = DB::select('datetime', 'sunlight')->from('quarter_hour')->order_by('datetime', 'DESC')->limit(20)->offset(0);
         return $query->execute()->as_array();
