a935e387ce37e4ef55da4e32bb921281ca4ea76c
Eric Turcotte <when.the.music.play@gmail.com>
2014-10-21 22:46:28 -0400

    Add some language files for future translations

---
+++ application/i18n/fr.php
@@ -0,0 +1,32 @@
+
+<?php
+
+return array(
+    'Dashboard'                 => 'Tableau de bord',
+    'Task list'                 => 'Liste des tâches',
+    'No task in the to do list' => 'Aucune tâche dans la liste',
+    'History'                   => 'Historique',
+    'Logout'                    => 'Déconnexion',
+    'Temperature'               => 'Température',
+    'Room temperature'          => 'Température de la pièce',
+    'Tank temperature'          => 'Température de l\'aquarium',
+    'Hot'                       => 'Chaud',
+    'Cold'                      => 'Froid',
+    'Value in Celsius (°C)'     => 'Valeur en Celsius (°C)',
+    'Sunlight'                  => 'Ensoleillement',
+    'Sunlight (%)'              => 'Ensoleillement (%)',
+    'Day'                       => 'Jour',
+    'Night'                     => 'Nuit',
+    'Value in percent (%)'      => 'Valeur en pourcentage (%)',
+    'Last communication'        => 'Dernière communication',
+
+
+
+    // Highcharts - START
+    'hc-months'                 => "['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre']",
+    'hc-shortMonths'            => "['Jan','Fév','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre']",
+    'hc-weekdays'               => "['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi']",
+    'hc-decimalPoint'           => ','
+
+    // Highcharts - END
+);
