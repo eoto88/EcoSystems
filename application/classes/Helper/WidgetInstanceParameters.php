bec3d87f4558020fc3b3f68b98aa617a35c23d34
Eric Turcotte <when.the.music.play@gmail.com>
2017-01-10 16:07:31 -0500

    Widget Instance Parameters

--- application/classes/Helper/WidgetInstanceParameters.php
+++ application/classes/Helper/WidgetInstanceParameters.php
@@ -5,33 +5,45 @@ class Helper_WidgetInstanceParameters {
     public function getView( $id_instance) {
         $hForm = new Helper_Form();
         $mParam = new Model_Param();
+        $mParamType = new Model_ParamType();
         $vParameters = null;
+        $arrParamTypes = array();
+
+        foreach($mParamType->getParamTypes() as $paramType) {
+            $arrParamTypes[$paramType['id']] = $paramType['title'];
+        }
 
         $form = $hForm->createForm(array(
             'name' => 'form-parameter',
             'fields' => array(
-               array(
-                   'name' => 'id',
-                   'type' => 'hidden'
-               ),
-               array(
-                   'name' => 'alias',
-                   'label' => __('Alias'),
-                   'maxlength' => 25
-               ),
-               array(
-                   'name' => 'title',
-                   'label' => __('Title'),
-                   'maxlength' => 100
-               ),
-               array(
-                   'type' => 'textarea',
-                   'name' => 'options',
-                   'label' => __('Options')
-               ),
-               array(
-                   'type' => 'submit'
-               )
+                array(
+                    'name' => 'id',
+                    'type' => 'hidden'
+                ),
+                array(
+                    'name' => 'alias',
+                    'label' => __('Alias'),
+                    'maxlength' => 25
+                ),
+                array(
+                    'name' => 'title',
+                    'label' => __('Title'),
+                    'maxlength' => 100
+                ),
+                array(
+                    'type' => 'select',
+                    'name' => 'id_type',
+                    'label' => __('Type'),
+                    'values' => $arrParamTypes
+                ),
+                array(
+                    'type' => 'textarea',
+                    'name' => 'options',
+                    'label' => __('Options')
+                ),
+                array(
+                    'type' => 'submit'
+                )
             )
         ));
 
@@ -41,21 +53,5 @@ class Helper_WidgetInstanceParameters {
         ));
 
         return $vParameters->render();
-    }
-
-    function getTodoLi($checked = false) {
-        $icon = 'fa-square-o';
-        if($checked) {
-            $icon = 'fa-check-square-o';
-        }
-        $dropdown ='<div class="dropdown">'.
-            '<button class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></button>'.
-            '<ul class="dropdown-menu js-status-update pull-right">'.
-            '<li><a href="#" class="action-edit"><i class="fa fa-pencil"></i>&nbsp;'. __('Edit') .'</a></li>'.
-            '<li><a href="#" class="action-delete"><i class="fa fa-trash-o"></i>&nbsp;'. __('Delete') .'</a></li>'.
-            '</ul></div>';
-        $edit = '<span class="actions">'. $dropdown .'</span>';
-        $title = '<span><i class="fa '. $icon .' check-icon"></i><span class="todo-title">{{title}}</span>'.$edit.'</span>';
-        return '<li class="todo" data-id="{{id}}" data-id-instance="{{id_instance}}">'.$title.'<span class="clearfix"></span></li>';
     }
 }
